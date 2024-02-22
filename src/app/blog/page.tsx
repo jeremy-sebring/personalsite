d6a4e3574cd6d4ad77b8a59ee00226e75654ff67
jeremy-sebring <10858588+jeremy-sebring@users.noreply.github.com>
2024-02-22 14:37:34 -0500

    basic posts page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,47 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card";
+import { getPosts } from "../data";
+
+import Link from "next/link";
+import { Suspense } from "react";
+  
+async function ReadyPosts() {
+    const posts = await getPosts();
+    return (
+      <div className="w-2/3 self-center mx-10 grid grid-cols-3 gap-3 ">
+        {posts.map((post) => (
+          <Card className="" key={post.id}>
+            <CardHeader>
+              <CardTitle><Link href={`/blog/${post.slug}`}>{post.title}</Link></CardTitle>
+            </CardHeader>
+            <CardContent>
+                lorem ipsum
+            </CardContent>
+            <CardFooter>
+              <p className="text-slate-400 text-sm"> Updated @ {post.updated_at.toString()}</p>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    );
+}  
+
+export default function Blog() {
+
+    return (
+        <div>
+            <h1>Blog</h1>
+            <Suspense fallback={<p>Loading...</p>}>
+                <ReadyPosts />
+            </Suspense>
+        </div>
+    )
+
+    
+}
